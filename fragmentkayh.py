0dff5917972c148b7671e42d1b3027d73bbed36b
KayHGNG <87310767+kayhgng@users.noreply.github.com>
2024-03-31 00:35:35 +0330

    Add files via upload

---
+++ fragmentkayh.py
@@ -0,0 +1,28 @@
+from rich import *
+print("""[green]
+  _  __              _    _    _____  _   _   _____ 
+ | |/ /             | |  | |  / ____|| \ | | / ____|
+ | ' /  __ _  _   _ | |__| | | |  __ |  \| || |  __ 
+ |  <  / _` || | | ||  __  | | | |_ || . ` || | |_ |
+ | . \| (_| || |_| || |  | | | |__| || |\  || |__| |
+ |_|\_\\\__,_| \__, ||_|  |_|  \_____||_| \_| \_____|    
+               __/ |                                
+              |___/                                       
+      
+           Github: https://github.com/kayhgng
+[/green]""")
+
+config_tempklate = """"
+https://fragment.first-to-the-egg.workers.dev/frag/ip/[worker]/uuid/host/port/path
+"""
+
+worker = input("Please Enter your worker's address without htttps and /:[/red]")
+uuid = input("Please Enter your uuid:")
+host = input("Please enter the host name:")
+port = input("Please Enter your host's port:")
+path = input("Please Enter your path :") 
+
+subsctiption = config_tempklate.replace("[worker]", worker).replace("uuid", uuid).replace("host", host).replace("port", port).replace("path", path)
+
+print("[green]Here is your subscription Group url:")
+print (subsctiption)
