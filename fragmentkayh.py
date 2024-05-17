018592fe80890b8eab23edb1b425ce2214fa2648
KayHGNG <87310767+kayhgng@users.noreply.github.com>
2024-05-17 21:36:13 +0330

    Update fragmentkayh.py

--- fragmentkayh.py
+++ fragmentkayh.py
@@ -1,5 +1,4 @@
-from rich import *
-print("""[green]
+print("""
   _  __              _    _    _____  _   _   _____ 
  | |/ /             | |  | |  / ____|| \ | | / ____|
  | ' /  __ _  _   _ | |__| | | |  __ |  \| || |  __ 
@@ -10,13 +9,13 @@ print("""[green]
               |___/                                       
       
            Github: https://github.com/kayhgng
-[/green]""")
+""")
 
 config_tempklate = """"
 https://fragment.first-to-the-egg.workers.dev/frag/ip/[worker]/uuid/host/port/path
 """
 
-worker = input("Please Enter your worker's address without htttps and /:[/red]")
+worker = input("Please Enter your worker's address without htttps and /:")
 uuid = input("Please Enter your uuid:")
 host = input("Please enter the host name:")
 port = input("Please Enter your host's port:")
@@ -24,5 +23,5 @@ path = input("Please Enter your path :")
 
 subsctiption = config_tempklate.replace("[worker]", worker).replace("uuid", uuid).replace("host", host).replace("port", port).replace("path", path)
 
-print("[green]Here is your subscription Group url:")
+print("[Here is your subscription Group url:")
 print (subsctiption)
